829a29178d656a3bd3822df44575f364db75af4b
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2026-01-12 19:59:04 +0200

    chore: create base project structure

---
+++ app/not-found.tsx
@@ -0,0 +1,31 @@
+import css from "./Home.module.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 - Page not found",
+  description: "Sorry, the page you are looking for does not exist.",
+  
+  openGraph: {
+    title: "404 - Page not found",
+    description: "Sorry, the page you are looking for does not exist.",
+    url: "https://Petl💛ve.com/not-found",
+    siteName: "Petl💛ve",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "404 - Page not found",
+      },
+    ],
+    type: "website",
+  },
+};
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1 className={css.title}>404 - Page not found</h1>
+            <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+        </div>);
+}
